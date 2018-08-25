f6ed4cb5a2534668e53d65270409266ceb846a5d
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-08-25 08:50:12 -0500

    done with part 2

--- bamazonManager.js
+++ bamazonManager.js
@@ -29,7 +29,7 @@ const displayLowInventory = () => {
   connection.query(queryString, (err, resp) => {
     if (err) throw err;
     // console.log(resp);
-    if (resp != []) {
+    if (resp.length != 0) {
       // console.log("We in this B");
       resp.map(x => {
         console.log("Displaying low-stock inventory");
